3097dbbb5918843fe96ce60d2b22185198971abb
kkfarnsworth <71725211+kkfarnsworth@users.noreply.github.com>
2024-06-09 03:35:00 -0400

    added story files

---
+++ story_content/6GDUHbbJVuM_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.22.17236.0%0D%0A%0D%0A00:00:00.288%20--%3E%2000:00:01.516%0D%0A[Music%20playing]%0D%0A%0D%0A00:00:06.305%20--%3E%2000:00:08.160%0D%0A%0D%0A%0D%0A00:00:08.192%20--%3E%2000:00:12.896%0D%0A%0D%0A%0D%0A"}')
